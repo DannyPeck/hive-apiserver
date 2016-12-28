76e31b5e43550ab25e2d5eda8e88923af55d6402
Danny Peck <danieljpeck93@gmail.com>
2016-12-27 22:39:53 -0600

    refactored MessageController to use query parameters and prepareOptions

--- app/routers/v1/admin/OrganizationRouter.js
+++ app/routers/v1/admin/OrganizationRouter.js
@@ -1,9 +1,6 @@
 // OrganizationRouter
 module.exports = exports = {
   '/organizations' : {
-    '/messages': {
-      get: { action: 'MessageController@getMessagesByOrg' }
-    },
     resource: {
       controller: 'OrganizationController'
     }

cc3d529385e11b3041645e613f193237aac6d10f
Danny Peck <danieljpeck93@gmail.com>
2016-12-30 13:00:06 -0600

    refactored IndexRouter /organizations route to use resource statment

--- app/routers/IndexRouter.js
+++ app/routers/IndexRouter.js
@@ -3,6 +3,9 @@ var passport = require ('passport');
 module.exports = exports = {
   '/v1': passport.authenticate ('bearer', { session: false }),
   '/organizations': {
-    post: { action: 'OrganizationController@create' }
+    resource: {
+      controller: 'OrganizationController',
+      allow: ['create']
+    }
   }
 };
